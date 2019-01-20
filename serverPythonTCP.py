c37da228fdc0bf5c4338eda94173e538c011186c
Unknown <59011344@kmitl.ac.th>
2019-01-20 16:04:37 +0700

    Client and Server Code

---
+++ serverPythonTCP.py
@@ -0,0 +1,22 @@
+# from socket import *
+import socket
+
+HOST = '127.0.0.1'   # or 127.0.0.1 or localhost
+PORT = 2600
+ADDR = (HOST,PORT)
+BUFFER = 4096
+
+#create a socket (SRV)
+#see python docs for socket for more info
+
+srv = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#bind socket to address
+srv.bind((ADDR))	#double parens create a tuple with one object
+srv.listen(5) # maximum queued connections is 5
+a = True
+while a:
+    conn,addr = srv.accept() #accepts the connection
+    print ('...connected!')
+    conn.sendall('Wohhhowwwww'.encode('utf-8'))
+conn.close()
